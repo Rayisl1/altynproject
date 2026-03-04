6e34777529cc52e2e5430686c7793230b4c46ccf
nualdaber <nualdaber@noreply.platform.alem.school>
2026-03-04 19:50:57 +0000

    Удалить  column-commander.sql

---  column-commander.sql
+++
@@ -1,6 +0,0 @@
-SELECT first_name, last_name FROM employees
-WHERE department_id = 1 ORDER BY first_name ASC;
-SELECT email AS contact_info, position AS job_title FROM employees
-WHERE position = 'Lead Engineer';
-SELECT first_name || last_name AS given_name, salary AS monthly_income FROM employees
-ORDER BY monthly_income DESC, given_name ASC;
