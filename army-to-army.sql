49fdd9d37d8fcd3ff6064e82192265cd6b4e67fb
nualdaber <nualdaber@noreply.platform.alem.school>
2026-03-10 18:14:09 +0000

    a

---
+++ army-to-army.sql
@@ -0,0 +1,4 @@
+SELECT playlists.Name AS PlaylistName, tracks.Name AS TrackName FROM playlists
+INNER JOIN playlist_track ON playlists.PlaylistId = playlist_track.PlaylistId
+INNER JOIN tracks ON playlist_track.TrackId = tracks.TrackId
+ORDER BY PlaylistName, TrackName;
